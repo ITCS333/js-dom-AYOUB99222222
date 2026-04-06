dc2ed54ea991af238bd4fb58867fef1beb6f8cd4
AYOUB99222222 <ayoubalansari99@hotmail.com>
2026-04-06 21:25:32 +0300

    Update index.js

--- index.js
+++ index.js
@@ -32,6 +32,18 @@ function highlightParagraph() {
  * Selects the element with the id 'to-be-removed' and removes it from the DOM.
  */
 function removeElement() {
-  const element = document.getElementById('to-be-removed');
-  if (element) element.remove();
+  document.getElementById('to-be-removed').remove();
+}
+
+
+// Do not edit the lines below.
+// These lines are for testing purposes.
+if (typeof module !== 'undefined' && module.exports) {
+    module.exports = {
+        changeHeadingText,
+        changeBoxColor,
+        addNewItem,
+        highlightParagraph,
+        removeElement
+    };
 }
